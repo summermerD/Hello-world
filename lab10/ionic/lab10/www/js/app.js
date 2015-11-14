c88769ae8444032d2f1fcaeaf17f1c778fa95691
summermerD <txtd4@mail.umkc.edu>
2015-11-13 20:51:35 -0600

    lab10

---
+++ lab10/ionic/lab10/www/js/app.js
@@ -0,0 +1,37 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.routes'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+.controller('pageCtrl', function($scope, $http) {
+    
+    $scope.getResult = function(searchKey, searchURL) {
+        var url= 'http://alechemydemo.mybluemix.net/api/Alchemy/search/'+searchKey+'/'+searchURL;
+        console.log(url);
+        $http.get(url) 
+            .success(function(data) { 
+                console.log(data);
+                $scope.result = data;
+            }) 
+            .error(function(err) { 
+                console.log("data not received from url");
+            }); 
+        }
+});
